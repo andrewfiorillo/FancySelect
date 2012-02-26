042519d28c75fa5acb8ff0145be76b1f7fc57899
andrewfiorillo <fiorilloaj@gmail.com>
2012-02-26 12:24:53 -0500

    cross-browser fixes

--- js/fancyselect.js
+++ js/fancyselect.js
@@ -1,19 +1,18 @@
 $.fn.fancyselect = function() {
 
 	var browser = "";
-	if($.browser.webkit) browser = "webkit";
-	else if ($.browser.mozilla) browser = "mozilla";
-	else if ($.browser.msie) browser = "msie";
-	else if ($.browser.opera) browser = "opera";
+	if ($.browser.webkit) browser = "webkit";
 
-    this.each(function(i,select){		
-        $(select).wrap('<span class="fancyselect ' + browser + ' ' + navigator.platform + '" />');
-        $(select).before('<span class="arrows" />');
-        $(select).focus(function(){
-            $(this).parent('span').addClass('focus');
-        });
-        $(select).blur(function(){
-            $(this).parent('span').removeClass('focus');
-        });
-    });
+	this.each(function(i,select){		
+		$(select).wrap('<span class="fancyselect ' + browser + '" />');
+		$(select).before('<span class="arrows" />');
+		$(select).focus(function(){ $(this).parent('span').addClass('focus') });
+		$(select).blur(function(){ $(this).parent('span').removeClass('focus') });				
+		if(browser != "webkit") {
+			$(select).fadeTo(0,0).before('<span class="selecttext" />');
+			$(select).bind("change keyup", function() { $(this).siblings(".selecttext").text($(select).val()) });
+			$(this).siblings(".selecttext").text($(select).val());
+		}
+	});
+
 };
