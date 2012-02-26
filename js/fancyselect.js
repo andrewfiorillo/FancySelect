0eebe4866de58be64263946412554f5dd0b26ac3
andrewfiorillo <fiorilloaj@gmail.com>
2012-02-25 20:21:26 -0500

    separated styles. browser detection for styling

--- js/fancyselect.js
+++ js/fancyselect.js
@@ -1,6 +1,13 @@
 $.fn.fancyselect = function() {
+
+	var browser = "";
+	if($.browser.webkit) browser = "webkit";
+	else if ($.browser.mozilla) browser = "mozilla";
+	else if ($.browser.msie) browser = "msie";
+	else if ($.browser.opera) browser = "opera";
+
     this.each(function(i,select){
-        $(select).wrap('<span class="fancyselect" />');
+        $(select).wrap('<span class="fancyselect ' + browser +'" />');
         $(select).before('<span class="arrows" />');
         $(select).focus(function(){
             $(this).parent('span').addClass('focus');
