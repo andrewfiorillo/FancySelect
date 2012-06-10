aea56cb62a6ff26c80c208f21ffcbfd2d0b2c941
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-10 17:18:14 -0400

    handle disabled selects and added methods to disable/enable. browser fixes

--- js/fancyselect.js
+++ js/fancyselect.js
@@ -8,19 +8,16 @@
 				.before('<span class="arrows" />')
 				.before('<span class="selecttext" />')
 				.fadeTo(0,0)
-				.focus(function() { $(this).parent('span').addClass('focus'); })
-				.blur(function() { $(this).parent('span').removeClass('focus'); })
-				.on("change keyup", function() { $(this).prev(".selecttext").text($(select).val()); })
+				.focus(function() { $(this).parent('span').addClass('focus') })
+				.blur(function() { $(this).parent('span').removeClass('focus') })
+				.on("change keyup", function() { $(this).prev(".selecttext").text($(select).val()) })
 				.siblings(".selecttext").text($(select).val());
 				
 			select.disable = function() {
-				$(this).attr("disabled", "disabled");
-				$(this).parent().addClass('disabled');
-			};
-			
+				$(this).attr("disabled", "disabled").parent().addClass('disabled');
+			};			
 			select.enable = function() {
-				$(this).removeAttr('disabled');
-				$(this).parent().removeClass('disabled');
+				$(this).removeAttr('disabled').parent().removeClass('disabled');
 			};
 			
 			if ($(select).attr("disabled")) select.disable();
