82bf1f6647555e602fc0cee05ed5bdd96bd94bab
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-10 17:03:31 -0400

    handle disabled selects and added methods to disable/enable. browser fixes

--- js/fancyselect.js
+++ js/fancyselect.js
@@ -1,28 +1,32 @@
-$.fn.fancyselect = function() {
+(function($){
+	
+	$.fn.fancyselect = function() {
 
-	var browser = "";
-	if ($.browser.webkit) browser = "webkit";
-
-	this.each(function(i,select) {		
-		
-		$(select).wrap('<span class="fancyselect ' + browser + '" />');
-		$(select).before('<span class="arrows" />');
-		
-		$(select).focus(function() {
-			$(this).parent('span').addClass('focus');
+		this.each(function(i,select) {	
+			$(select)
+				.wrap('<span class="fancyselect" />')
+				.before('<span class="arrows" />')
+				.before('<span class="selecttext" />')
+				.fadeTo(0,0)
+				.focus(function() { $(this).parent('span').addClass('focus'); })
+				.blur(function() { $(this).parent('span').removeClass('focus'); })
+				.on("change keyup", function() { $(this).prev(".selecttext").text($(select).val()); })
+				.siblings(".selecttext").text($(select).val());
+				
+			select.disable = function() {
+				$(this).attr("disabled", "disabled");
+				$(this).parent().addClass('disabled');
+			};
+			
+			select.enable = function() {
+				$(this).removeAttr('disabled');
+				$(this).parent().removeClass('disabled');
+			};
+			
+			if ($(select).attr("disabled")) select.disable();
+			
 		});
-		$(select).blur(function() {
-			$(this).parent('span').removeClass('focus');
-		});
-		
-		if(browser != "webkit") {
-			$(select).fadeTo(0,0).before('<span class="selecttext" />');
-			$(select).bind("change keyup", function() {
-				$(this).siblings(".selecttext").text($(select).val());
-			});
-			$(this).siblings(".selecttext").text($(select).val());
-		}
-		
-	});
 
-};
+	};
+
+})( jQuery );
