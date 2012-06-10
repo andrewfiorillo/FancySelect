e69d0d4529d04ac0f209f628effaafa3d7e6bd50
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-10 12:56:09 -0400

    removed verdana. formatting

--- js/fancyselect.js
+++ js/fancyselect.js
@@ -3,16 +3,26 @@ $.fn.fancyselect = function() {
 	var browser = "";
 	if ($.browser.webkit) browser = "webkit";
 
-	this.each(function(i,select){		
+	this.each(function(i,select) {		
+		
 		$(select).wrap('<span class="fancyselect ' + browser + '" />');
 		$(select).before('<span class="arrows" />');
-		$(select).focus(function(){ $(this).parent('span').addClass('focus') });
-		$(select).blur(function(){ $(this).parent('span').removeClass('focus') });				
+		
+		$(select).focus(function() {
+			$(this).parent('span').addClass('focus');
+		});
+		$(select).blur(function() {
+			$(this).parent('span').removeClass('focus');
+		});
+		
 		if(browser != "webkit") {
 			$(select).fadeTo(0,0).before('<span class="selecttext" />');
-			$(select).bind("change keyup", function() { $(this).siblings(".selecttext").text($(select).val()) });
+			$(select).bind("change keyup", function() {
+				$(this).siblings(".selecttext").text($(select).val());
+			});
 			$(this).siblings(".selecttext").text($(select).val());
 		}
+		
 	});
 
 };
