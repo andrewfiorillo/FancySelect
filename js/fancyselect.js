651baab1815e5ab7a9e4aaa8ba22417bf3eb1782
andrewfiorillo <fiorilloaj@gmail.com>
2012-06-17 16:30:10 -0400

    init/destroy/enable/disable methods. 2x arrow graphic. cleanup.

--- js/fancyselect.js
+++ js/fancyselect.js
@@ -1,29 +1,52 @@
 (function($){
 	
-	$.fn.fancyselect = function() {
-
-		this.each(function(i,select) {	
-			$(select)
-				.wrap('<span class="fancyselect" />')
-				.before('<span class="arrows" />')
-				.before('<span class="selecttext" />')
-				.fadeTo(0,0)
-				.focus(function() { $(this).parent('span').addClass('focus') })
-				.blur(function() { $(this).parent('span').removeClass('focus') })
-				.on("change keyup", function() { $(this).prev(".selecttext").text($(select).val()) })
-				.siblings(".selecttext").text($(select).val());
-				
-			select.disable = function() {
-				$(this).attr("disabled", "disabled").parent().addClass('disabled');
-			};			
-			select.enable = function() {
-				$(this).removeAttr('disabled').parent().removeClass('disabled');
-			};
-			
-			if ($(select).attr("disabled")) select.disable();
-			
-		});
-
+	var methods = {
+		init: function() {
+			return this.each(function(i,el) {
+				$(el).wrap('<span class="fancyselect" />')
+					.before('<span class="arrows" />')
+					.before('<span class="selecttext" />')
+					.fadeTo(0,0)
+					.focus(function() { $(el).parent('span').addClass('focus'); })
+					.blur(function() { $(el).parent('span').removeClass('focus'); })
+					.on("change keydown keyup", function() { $(el).fancyselect('update'); })
+					.fancyselect('update');
+				if ($(el).attr("disabled")) $(el).fancyselect('disable');
+			});
+		},
+		destroy: function() {
+			return this.each(function(i,el) {
+				$(el).siblings('span').remove();
+				$(el).unwrap().fadeTo(0,1).unbind('focus blur change keydown keyup');
+			});
+		},
+		enable: function() {
+			return this.each(function(i,el) {
+				$(el).removeAttr('disabled').parent().removeClass('disabled');
+			});
+		},
+		disable: function() {
+			return this.each(function(i,el) {
+				$(el).attr("disabled", "disabled").parent().addClass('disabled');
+			});
+		},
+		update: function() {
+			return this.each(function(i,el) {
+				$(el).prev(".selecttext").text($(el).children('option:selected').text());
+			});
+		}
+	};
+	
+	$.fn.fancyselect = function(method) {
+  		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		}
+		else if (typeof method === 'object' || ! method) {
+			return methods.init.apply(this, arguments);
+		}
+		else {
+			$.error('Method ' + method + ' does not exist for .fancyselect()');
+		}
 	};
 
 })( jQuery );
